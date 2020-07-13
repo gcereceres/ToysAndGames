c9ea766529347d0f2d516316474cdbeb02ec2b89
Gustavo Cereceres <gcereceres@gmail.com>
2020-07-13 13:30:10 -0600

    Adding Product Model

---
+++ ToysAndGames/ToysAndGames.Dal/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ToysAndGames.Dal.Models
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(100)]
+        public string Description { get; set; }
+
+        [Range(0, 100)]
+        public int AgeRestriction { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Company { get; set; }
+
+        [Required]
+        [Range(1, 1000)]
+        public decimal Price { get; set; }
+    }
+}
