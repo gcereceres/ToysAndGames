ca91ee028c971e8e239350ce94a801599c8b2b94
Gustavo Cereceres <gcereceres@gmail.com>
2020-07-13 14:09:32 -0600

    Adding data context

---
+++ ToysAndGames/ToysAndGames.Dal/ToysAndGamesContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ToysAndGames.Dal.Models;
+
+namespace ToysAndGames.Dal
+{
+    public class ToysAndGamesContext : DbContext
+    {
+        public ToysAndGamesContext(DbContextOptions<ToysAndGamesContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
