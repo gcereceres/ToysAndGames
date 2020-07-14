9a9e915f1a69ffac75a002deaa8cfc04a2ba2350
Gustavo Cereceres <gcereceres@gmail.com>
2020-07-14 15:27:37 -0600

    Adding ProductController with GetProducts method

---
+++ ToysAndGames/ToysAndGames.Web/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToysAndGames.Dal;
+using ToysAndGames.Web.ViewModels;
+
+namespace ToysAndGames.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public ActionResult GetProducts()
+        {
+            return Ok(_productRepository.GetProducts());
+        }
+    }
+}
