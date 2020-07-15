88bf8426ddd9ae54992950e5ae291381a04afdf0
Gustavo Cereceres <gcereceres@gmail.com>
2020-07-14 23:23:33 -0600

    Adding starter productList & productInput compnts

--- ToysAndGames/ToysAndGames.Web/wwwroot/ClientApp/src/app/app.module.ts
+++ ToysAndGames/ToysAndGames.Web/wwwroot/ClientApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListingComponent } from './product-listing/product-listing.component';
+import { ProductInputComponent } from './product-input/product-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListingComponent,
+    ProductInputComponent
   ],
   imports: [
     BrowserModule,
