fba78682a14af52a76e7ba420f333b63d5c6406f
Gustavo Cereceres <gcereceres@gmail.com>
2020-07-15 13:16:15 -0600

    Remove unnecesary using

--- ToysAndGames/ToysAndGames.Web/Controllers/ProductController.cs
+++ ToysAndGames/ToysAndGames.Web/Controllers/ProductController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ToysAndGames.Dal;
 using ToysAndGames.Dal.Models;
-using ToysAndGames.Web.ViewModels;
 
 namespace ToysAndGames.Web.Controllers
 {
