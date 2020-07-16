e812b8eda3b3365fe9ce4477a193f758e5a35b69
Gustavo Cereceres <gcereceres@gmail.com>
2020-07-15 20:12:38 -0600

    Adding Product Service

--- ToysAndGames/ToysAndGames.Web/wwwroot/ClientApp/src/app/product.service.ts
+++ ToysAndGames/ToysAndGames.Web/wwwroot/ClientApp/src/app/product.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Product } from './product';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  toysAndGamesUrl = 'https://localhost:44354/api';
+  toysAndGamesUrl = 'https://localhost:44354/api/product';
 
   constructor(private http: HttpClient) { }
 
   getProduct(){
-    return this.http.get(`${this.toysAndGamesUrl}/product`);
+    return this.http.get(this.toysAndGamesUrl);
+  }
+
+  createProduct(product:Product){
+    return this.http.post(this.toysAndGamesUrl, product);
   }
 }
